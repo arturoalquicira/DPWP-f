be18780a5216375a832746b3a4a3e403f4b8f054
arturoalquicira <arturoalquicira@fullsail.edu>
2014-03-04 23:06:33 -0500

    start- global variables

---
+++ lab1-madlib/main.py
@@ -0,0 +1,12 @@
+"""
+Global Variables
+"""
+name = raw_input("Your name: ")
+hometown = raw_input("Your hometown: ")
+noun = raw_input("Noun: ")
+your_age = raw_input("Your age: ")
+random_number = raw_input("Random number: ")
+lucky_number = raw_input("Your lucky number: ")
+
+
+
