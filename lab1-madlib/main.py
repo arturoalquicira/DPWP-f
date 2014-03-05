ea547448f5a5558245a3d27bc192df9ec078fef4
arturoalquicira <arturoalquicira@fullsail.edu>
2014-03-05 17:35:10 -0500

    function - float, int

--- lab1-madlib/main.py
+++ lab1-madlib/main.py
@@ -1,4 +1,13 @@
 """
+Arturo Alquicira
+DPWP
+
+Mad Lib
+
+"""
+
+
+"""
 Global Variables
 """
 name = raw_input("Your name: ")
@@ -7,6 +16,20 @@ noun = raw_input("Noun: ")
 your_age = raw_input("Your age: ")
 random_number = raw_input("Random number: ")
 lucky_number = raw_input("Your lucky number: ")
+
+"""
+Float
+"""
+
+def year_of_birth(age, this_year):
+    y = this_year - age
+    return y
+
+actual_year = 2014
+
+year = year_of_birth(int(your_age), actual_year)
+
+print year
 
 
 
